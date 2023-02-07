4ae55f0d7a9c6b591ffb3475f2d90df361358a42
Julio <julio.bermudez619@gmail.com>
2023-02-07 08:56:03 -0800

    Created a user interface for rps game with js and css included

--- rps.js
+++ rps.js
@@ -1,67 +1,99 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = document.querySelectorAll('button');  ///['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
+choices.forEach(choice => choice.addEventListener('click', playGame));
 
 
-function game(){
-    for (let i = 1; i <= 5; i++){
-        console.log(`Round: ${i}`);
-        playRound();
-        console.log(`Player: ${playerScore} || Computer:${computerScore}`);
-    }
-
-    console.log('Final Result');
-
-    if (playerScore > computerScore){
-        console.log('Player won the game!');
-    } else if ( computerScore > playerScore){
-        console.log('Computer won the game.');
-    }
-
-    console.log(`Player: ${playerScore} || Computer:${computerScore}`);
-
-}
-
-function playRound(){
-    const playerSelection = playerChoice();
+function playGame(e){
+    const playerSelection = e.target.id;
     const computerSelection = computerChoice(); 
-    let winner = roundWinner(playerSelection, computerSelection);
-    console.log(`Player choice: ${playerSelection}`);
-    console.log(`Computer choice: ${computerSelection}`);
-    console.log(winner);
-   
-    
+    roundWinner(playerSelection, computerSelection.textContent.toLowerCase());
+    document.querySelector('.computer-choice').textContent = `Computer choice: ${computerSelection.textContent}`;
+    document.querySelector('.scoreboard').textContent = `Player Score: ${playerScore} || Computer Score: ${computerScore}`;
 
+    if (playerScore >= 5){
+        return document.querySelector('.winner').textContent= `You're the winner of this game !`;
+    } else if (computerScore >=5){
+        return document.querySelector('.winner').textContent= `The computer is the winner of this game`;
+    }
 } 
 
-function playerChoice(){
-    let input = prompt('Select Rock, Paper or Scissors.');
-    input = input.toLowerCase();
-    return input;
-
-}
 
 function computerChoice(){
-    return choices[Math.floor(Math.random()*choices.length)];
-
+    rand = choices[Math.floor(Math.random()*choices.length)];
+    return rand;
+    
 }
 
 function roundWinner(choiceP,choiceC){
     if (choiceP === choiceC){
-        return 'This round is a tie';
+        return document.querySelector('.round-result').textContent = 'Result: Tie';
     } else if (
         (choiceP === 'rock' && choiceC === 'scissors')||
         (choiceP === 'scissors' && choiceC === 'paper')||
         (choiceP === 'paper' && choiceC ==='rock')
         ){
             playerScore++; 
-            return 'Player won this round!'; 
+            return document.querySelector('.round-result').textContent = 'Result: Player is the round winner!'; 
         } else {
             computerScore++;
-            return 'Computer won this round.';
+            return document.querySelector('.round-result').textContent = 'Result: Computer won this round.';
         }
 
 }
 
-game();
+
+
+/*
+function playerChoice(){
+
+  let input = prompt('Select Rock, Paper or Scissors.');  
+    input = input.toLowerCase();
+    return input;                     
+
+}
+*/
+
+
+///const btns = document.querySelectorAll('button');
+
+/*  querySelectorAll returns a node list and has no addEventListener method.
+    Node list behaves similaryly to an array. Code below selects all 3 buttons
+    and plays 3 rounds the moment the page is loaded.
+
+btns.forEach(()=>{
+    playRound();
+} )*/
+
+/*
+btn.addEventListener('click', () =>{
+    playRound();
+})
+*/
+
+/*
+function game(){
+        ///This function plays 5 round game.    
+        for (let i = 1; i <= 5; i++){
+            console.log(`Round: ${i}`);
+            playRound();
+            console.log(`Player: ${playerScore} || Computer:${computerScore}`);
+        }
+    
+        console.log('Final Result');
+    
+        if (playerScore > computerScore){
+            console.log('Player won the game!');
+        } else if ( computerScore > playerScore){
+            console.log('Computer won the game.');
+        }
+    
+        console.log(`Player: ${playerScore} || Computer:${computerScore}`);
+    
+    }
+    
+    
+    
+    }
+*/
